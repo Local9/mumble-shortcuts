77ef1b00071d9998a50806a7ffc782fd13e3f01a
local9 <apjcookie1@hotmail.com>
2025-10-01 15:44:06 +0100

    feat: added an edit feature

--- src/lib/components/widget/list.mumble-shortcut.svelte
+++ src/lib/components/widget/list.mumble-shortcut.svelte
@@ -3,9 +3,14 @@
   import { Button } from "$lib/components/ui/button/index.js";
   import { setItem } from "$lib/services/local-storage.service.js";
   import TrashIcon from "@lucide/svelte/icons/trash";
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import PencilIcon from "@lucide/svelte/icons/pencil";
 
+  let mumbleShortcut = $state<any>(null);
   let mumbleShortcuts = $state(JSON.parse(getItem("mumbleShortcuts") || "[]"));
   let draggedIndex = $state<number | null>(null);
+  let open = $state(false);
 
   function moveItem<T>(array: T[], fromIndex: number, toIndex: number): T[] {
     const copy = array.slice();
@@ -60,6 +65,25 @@
       mumbleShortcuts = JSON.parse(getItem("mumbleShortcuts") || "[]");
     }, 1000);
   });
+
+  function openMumbleShortcutEditSheet(shortcut: any) {
+    mumbleShortcut = shortcut;
+    open = true;
+  }
+
+  function updateMumbleShortcut(mumbleShortcut: any) {
+    if (mumbleShortcut.shortcutName === "" || mumbleShortcut.mumbleUrl === "") {
+      return;
+    }
+    
+    if (!mumbleShortcut.mumbleUrl.startsWith("mumble://")) {
+      return;
+    }
+
+    mumbleShortcuts = mumbleShortcuts.map((shortcut: any) => shortcut.id === mumbleShortcut.id ? mumbleShortcut : shortcut);
+    setItem("mumbleShortcuts", JSON.stringify(mumbleShortcuts));
+    open = false;
+  }
 </script>
 
 <div class="flex flex-col gap-2">
@@ -76,6 +100,9 @@
       role="listitem"
     >
       <Button variant="outline" onclick={() => {openMumbleShortcut(mumbleShortcut)}} class="grid-cols-1 justify-start self-start w-full">{mumbleShortcut.shortcutName}</Button>
+      <Button variant="outline" onclick={() => {openMumbleShortcutEditSheet(mumbleShortcut)}} class="grid-cols-1 justify-end self-end w-fit">
+        <PencilIcon />
+      </Button>
       <Button variant="outline" onclick={() => {removeMumbleShortcut(mumbleShortcut)}} class="grid-cols-1 justify-end self-end w-fit">
         <TrashIcon />
       </Button>
@@ -85,6 +112,25 @@
 
 <style>
   .grid-buttons {
-    grid-template-columns: 1fr auto;
+    grid-template-columns: 1fr auto auto;
   }
 </style>
+
+<Sheet.Root open={open}>
+  <Sheet.Content>
+    <Sheet.Header>
+      <Sheet.Title>Update Shortcut</Sheet.Title>
+      <Sheet.Description>
+        This action cannot be undone. This will permanently update your shortcut.
+      </Sheet.Description>
+    </Sheet.Header>
+    <div class="flex flex-col gap-2 p-4">
+      <Input type="text" placeholder="Shortcut Name" bind:value={mumbleShortcut.shortcutName} />
+      <Input type="text" placeholder="Mumble URL" bind:value={mumbleShortcut.mumbleUrl} />
+    </div>
+    <Sheet.Footer class="grid grid-cols-2 gap-2">
+      <Button variant="outline" onclick={() => {updateMumbleShortcut(mumbleShortcut)}} class="grid-cols-1 w-full">Update</Button>
+      <Button variant="outline" onclick={() => {open = false}} class="grid-cols-1 w-full">Cancel</Button>
+    </Sheet.Footer>
+  </Sheet.Content>
+</Sheet.Root>
