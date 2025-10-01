4c2969fb4c5f1c677d5481a111459064ece01eb8
local9 <apjcookie1@hotmail.com>
2025-10-01 18:27:13 +0100

    feat: added alert for information

---
+++ src/lib/components/widget/information.alert.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import * as Alert from "$lib/components/ui/alert/index.js";
+  import InfoIcon from "@lucide/svelte/icons/info";
+
+  let classes: string = $state("hidden");
+
+  $effect(() => {
+    classes = "absolute bottom-10 right-10 w-[500px]";
+    setTimeout(() => {
+      classes = "hidden";
+    }, 10000);
+  });
+
+</script>
+
+<Alert.Root variant="default" class={classes}>
+  <InfoIcon />
+  <Alert.Title>Information</Alert.Title>
+  <Alert.Description>
+    When you copy the Mumble Channel URL, you can paste on this page to add it to your shortcuts.
+  </Alert.Description>
+</Alert.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import MumbleShortcut from "$lib/components/widget/mumble-shortcut/mumble-shortcut.svelte";
+  import InformationAlert from "$lib/components/widget/information.alert.svelte";
 </script>
 
 <div class="flex flex-col gap-2"> 
   <MumbleShortcut />
 </div>
+
+<InformationAlert />
